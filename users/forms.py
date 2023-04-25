e36d735018e6201fadac9b3d50ec7af1958f45d5
beba2123 <antenehbeba@gmail.com>
2023-04-25 22:12:28 +0300

    updated commit

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
+
 
 class coustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -13,3 +15,7 @@ class coustomUserCreationForm(UserCreationForm):
             super(coustomUserCreationForm, self).__init__(*args, **kwargs)
             for name, field in self.fields.items():
                  field.widget.attrs.update({'class': 'input'})
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'

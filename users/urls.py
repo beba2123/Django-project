9dfdea6e0af431711747fefb2a31f8547ef64b27
beba2123 <antenehbeba@gmail.com>
2023-04-27 22:10:57 +0300

    updated commit in adding skill

--- users/urls.py
+++ users/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
 
     path('edit-account/', views.editAccount, name="edit-account"),
     path('create-skill/', views.createSkill, name="create-skill"),
+    path('update-skill/<str:pk>', views.createSkill, name="update-skill"),
 ]
